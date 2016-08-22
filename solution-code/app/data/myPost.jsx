13df751a63bb4853d8aadf14d50240af271ab50a
ZebGirouard <timzebgir@gmail.com>
2016-08-22 07:10:45 -0600

    Movin soln to other branch

--- solution-code/app/data/myPost.jsx
+++
@@ -1,7 +0,0 @@
-// Create Post object
-export default {
-  title: "My First Post",
-  author: "Mr Cat",
-  body: "Check it out. This is the first post on my dope blog!",
-  comments: ["First!", "Second!", "This blog needs more GIFs."]
-};
--- solution-code/readme.md
+++
@@ -1,4 +0,0 @@
-#Setup
-
-* Run `npm install` to install dependencies and start your `gulp` task (which can be run seperately at any time with just `gulp`)
-* Run `npm start` to start a `nodemon` server on `localhost:3000`
